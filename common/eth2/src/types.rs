1b957491ef8b739770f00d1f651348db9f906a67
Paul Hauner <paul@paulhauner.com>
2020-08-31 09:03:50 +1000

    Remove extra mod from types

--- common/eth2/src/types.rs
+++ common/eth2/src/types.rs
@@ -1,12 +1,13 @@
-mod validator_status;
-
 use serde::{Deserialize, Serialize};
 use std::fmt;
 use std::str::FromStr;
 use types::serde_utils;
 
-pub use types::{Checkpoint, Epoch, Fork, Hash256, PublicKeyBytes, Slot};
-pub use validator_status::{ValidatorData, ValidatorStatus};
+pub use types::{Checkpoint, Epoch, Fork, Hash256, PublicKeyBytes, Slot, Validator};
+
+/// The number of epochs between when a validator is eligible for activation and when they
+/// *usually* enter the activation queue.
+const EPOCHS_BEFORE_FINALITY: u64 = 3;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct GenesisData {
@@ -170,6 +171,69 @@ impl fmt::Display for ValidatorId {
         match self {
             ValidatorId::PublicKey(pubkey) => write!(f, "{:?}", pubkey),
             ValidatorId::Index(index) => write!(f, "{}", index),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ValidatorData {
+    #[serde(with = "serde_utils::quoted")]
+    pub index: u64,
+    #[serde(with = "serde_utils::quoted")]
+    pub balance: u64,
+    pub status: ValidatorStatus,
+    pub validator: Validator,
+}
+
+// TODO: make this match the spec.
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
+pub enum ValidatorStatus {
+    Unknown,
+    WaitingForEligibility,
+    WaitingForFinality(Epoch),
+    WaitingInQueue,
+    StandbyForActive(Epoch),
+    Active,
+    ActiveAwaitingExit(Epoch),
+    Exited(Epoch),
+    Withdrawable,
+}
+
+impl ValidatorStatus {
+    pub fn from_validator(
+        validator_opt: Option<&Validator>,
+        epoch: Epoch,
+        finalized_epoch: Epoch,
+        far_future_epoch: Epoch,
+    ) -> Self {
+        if let Some(validator) = validator_opt {
+            if validator.is_withdrawable_at(epoch) {
+                ValidatorStatus::Withdrawable
+            } else if validator.is_exited_at(epoch) {
+                ValidatorStatus::Exited(validator.withdrawable_epoch)
+            } else if validator.is_active_at(epoch) {
+                if validator.exit_epoch < far_future_epoch {
+                    ValidatorStatus::ActiveAwaitingExit(validator.exit_epoch)
+                } else {
+                    ValidatorStatus::Active
+                }
+            } else {
+                if validator.activation_epoch < far_future_epoch {
+                    ValidatorStatus::StandbyForActive(validator.activation_epoch)
+                } else if validator.activation_eligibility_epoch < far_future_epoch {
+                    if finalized_epoch < validator.activation_eligibility_epoch {
+                        ValidatorStatus::WaitingForFinality(
+                            validator.activation_eligibility_epoch + EPOCHS_BEFORE_FINALITY,
+                        )
+                    } else {
+                        ValidatorStatus::WaitingInQueue
+                    }
+                } else {
+                    ValidatorStatus::WaitingForEligibility
+                }
+            }
+        } else {
+            ValidatorStatus::Unknown
         }
     }
 }
