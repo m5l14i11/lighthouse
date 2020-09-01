8a5edd6e06a68391c711f8af72202ee0976efb53
Michael Sproul <michael@sigmaprime.io>
2020-09-01 18:18:19 +1000

    Add simple lower bound tests

--- validator_client/slashing_protection/src/lower_bound.rs
+++ validator_client/slashing_protection/src/lower_bound.rs
@@ -1,7 +1,7 @@
 use std::cmp;
 use types::{Epoch, Slot};
 
-#[derive(Debug, Clone, Copy, Default)]
+#[derive(Debug, Clone, Copy, Default, PartialEq)]
 pub struct LowerBound {
     pub block_proposal_slot: Option<Slot>,
     pub attestation_source_epoch: Option<Epoch>,
@@ -35,4 +35,45 @@ impl LowerBound {
     }
 }
 
-// FIXME(sproul): check update behaviour
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn update_nones() {
+        let all_none = LowerBound {
+            block_proposal_slot: None,
+            attestation_source_epoch: None,
+            attestation_target_epoch: None,
+        };
+        assert_eq!(all_none, LowerBound::default());
+        let all_some = LowerBound {
+            block_proposal_slot: Some(Slot::new(10)),
+            attestation_source_epoch: Some(Epoch::new(10)),
+            attestation_target_epoch: Some(Epoch::new(10)),
+        };
+        assert_eq!(all_none.update(all_some), all_some);
+        assert_eq!(all_some.update(all_none), all_some);
+    }
+
+    #[test]
+    fn simple() {
+        assert_eq!(
+            LowerBound {
+                block_proposal_slot: Some(Slot::new(1)),
+                attestation_source_epoch: Some(Epoch::new(0)),
+                attestation_target_epoch: Some(Epoch::new(100)),
+            }
+            .update(LowerBound {
+                block_proposal_slot: Some(Slot::new(0)),
+                attestation_source_epoch: Some(Epoch::new(96)),
+                attestation_target_epoch: Some(Epoch::new(98)),
+            }),
+            LowerBound {
+                block_proposal_slot: Some(Slot::new(1)),
+                attestation_source_epoch: Some(Epoch::new(96)),
+                attestation_target_epoch: Some(Epoch::new(100)),
+            }
+        );
+    }
+}
